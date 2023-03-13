7a8471b3ca7eeb56f88673a7c94efb430bd761e7
mukesh1996-ds <mks.mukesh1996@gmail.com>
2023-03-13 23:23:01 +0530

    module

---
+++ Python Codes/02_module.py
@@ -0,0 +1,18 @@
+# To install a module use pip install <model name>
+# I will install pandas now 
+
+# pip install pandas 
+
+# After installing how to use the module 
+import pandas as pd  
+
+# Pandas is used to load the dataset and manipulate it 
+
+df = pd.read_csv('Python Codes\peacock.csv')
+
+print(df.head)
+
+
+# Also you can check what are the built in module 
+
+# link: https://docs.python.org/3/py-modindex.html 
