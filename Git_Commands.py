9d43f5efddb68bcf8853779957d12e4ed232fefa
mukesh1996-ds <mks.mukesh1996@gmail.com>
2023-03-13 22:38:43 +0530

    Demo

---
+++ Git_Commands.py
@@ -0,0 +1,9 @@
+## In this python file I will be adding all the GitHub commands that are regulary needed
+
+"""
+git init --> This is used to start the connection between github and local system 
+git add * --> This is used to add the new create file to our git repo
+git commit -m "first commit" --> Command is used to commit the changes that are needed to push to repo
+git branch -M main --> Selecting the main branch
+git push -u origin main --> Finally push the data to our repo on cloud
+"""
