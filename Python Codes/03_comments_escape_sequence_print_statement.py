f2db387f31ee3bd27db355704e4038c505445f6f
mukesh1996-ds <mks.mukesh1996@gmail.com>
2023-03-21 10:10:17 +0530

    escape

--- Python Codes/03_comments_escape_sequence_print_statement.py
+++ Python Codes/03_comments_escape_sequence_print_statement.py
@@ -1,1 +1,34 @@
-print("Hello World \n I love india")
+# Single line comments 
+
+
+print("Single line comments")
+# This hash symbol is used for single line comments 
+
+
+"""
+Multiple line comments 
+
+What ever plased inside the quotes is called multiple line comments.
+
+"""
+
+# Escape Sequence 
+
+print("Hello world!, \n what was your learning from the last ")
+
+"""
+This \n represent that what ever you pass after that it will print in next line at the time of execution.
+"""
+
+
+# One Wonderful example 
+
+# Single line comments OR multiple line comments --> write it any were when we needed to define which operation or method used.
+
+
+a = 10
+
+if (a > 20):
+    print("{} is big".format(a))
+else: 
+    print("Not big")
