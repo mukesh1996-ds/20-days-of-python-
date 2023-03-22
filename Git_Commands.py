cfa37015cac17ac65ab018badff5966bb713d51d
mukesh1996-ds <mks.mukesh1996@gmail.com>
2023-03-22 09:48:02 +0530

    git

--- Git_Commands.py
+++ Git_Commands.py
@@ -1,6 +1,11 @@
 ## In this python file I will be adding all the GitHub commands that are regulary needed
 
 """
+# Config your GItHUB using the following
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+# Push the code to github
 git init --> This is used to start the connection between github and local system 
 git add * --> This is used to add the new create file to our git repo
 git commit -m "first commit" --> Command is used to commit the changes that are needed to push to repo
